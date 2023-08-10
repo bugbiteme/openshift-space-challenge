7966d6d50095aade5709fe7099d7a625bbfdc93d
Anthony Green <green@moxielogic.com>
2023-08-10 19:50:35 -0400

    Add player credentials to the cluster and generate handouts

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # catalyst
 
+This playbook requires the ``oc`` command.
 After logging into the OCP cluster as admin, run ``ansible-playbook deploy.yaml``.
+
+In addition to deploying CTFd, the playbook creates player accounts on OCP.
+Credentials are in ``credentials.txt``.
+Run ``make-user-creds.sh`` to generate a new batch credentials.
---
+++ make-user-creds.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# File to store the generated htpasswd entries
+HTPASSWD_FILE="players.htpasswd"
+
+# File to store the player and password pairs
+CREDENTIALS_FILE="credentials.csv"
+
+# Start a new credentials file
+echo "username,password" > $CREDENTIALS_FILE
+
+# Loop to generate 100 players
+for i in $(seq 1 100); do
+  # Generate random 8 character password
+  PASSWORD=$(cat /dev/urandom | tr -dc 'a-hj-km-np-zA-HJ-KM-NP-Z2-9' | head -c 8)
+
+  # Append to the htpasswd file
+  if [ "$i" -eq 1 ]; then
+    echo -n "$PASSWORD" | htpasswd -ciB $HTPASSWD_FILE player$i
+  else
+    echo -n "$PASSWORD" | htpasswd -iB $HTPASSWD_FILE player$i
+  fi
+
+  # Append to the credentials file
+  echo "player$i,$PASSWORD" >> $CREDENTIALS_FILE
+done
+
+latex credential-handouts.tex
+dvipdf credential-handouts.dvi
