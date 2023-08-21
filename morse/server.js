9bc27478130b43583568ede287a1d1c125c005c3
Anthony Green <green@moxielogic.com>
2023-08-21 17:54:15 -0400

    Updates

--- morse/server.js
+++ morse/server.js
@@ -1,4 +1,6 @@
-// Middleware to parse JSON
+const express = require('express');
+app = express();
+
 app.use(express.json());
 
 const CSV_TO_MORSE = process.env.MORSE_CSV;
@@ -7,21 +9,20 @@ const morseDict = {};
 // Parse CSV from environment variable into morseDict
 if (CSV_TO_MORSE) {
     const lines = CSV_TO_MORSE.split('\n');
-    console,
     lines.forEach(line => {
         const [char, morse] = line.split(',');
         morseDict[morse] = char;
     });
+    console.log(morseDict['...']);
 }
 
-app.post('/decode', (req, res) => {
-    const { morseMessage } = req.body;
-
-    if (!morseMessage) {
-        return res.status(400).send({ error: 'morseMessage is required' });
-    }
-
-    const decodedMessage = morseMessage.split(' ').map(morseChar => morseDict[morseChar] || '').join('');
-
+app.post('/decode-morse', (req, res) => {
+    const { message } = req.body;
+    message.split(' ').map(morseChar => console.log(morseChar))
+    const decodedMessage = message.split(' ').map(morseChar => morseDict[morseChar] || '').join('');
     return res.send({ decodedMessage });
+});
+
+app.listen(8080, '0.0.0.0', () => {
+    console.log('Server started on http://0.0.0.0:8080');
 });
