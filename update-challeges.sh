686f8603c12a6b2507a138ca79412c94f4f102f9
Marco Berube <mberube@gmail.com>
2024-02-27 11:40:42 -0500

    updated challenges
    removed need for judges for hello world and french connection

--- update-challeges.sh
+++ update-challeges.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sed 's/cluster-[^.]*\.[^.]*\.sandbox[^.]*\.opentlc\.com/{{ cluster_url }}/g' | jq
+#sed 's/cluster-[^.]*\.[^.]*\.sandbox[^.]*\.opentlc\.com/{{ cluster_url }}/g' | jq
+sed 's/cluster-[^.]*\.dynamic\.redhatworkshops\.io/{{ cluster_url }}/g' | jq
