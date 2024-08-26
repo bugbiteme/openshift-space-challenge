8951973fb6dc45085468d3e568a49ce57aeaccbb
Marco Berube <mberube@gmail.com>
2024-08-26 14:06:26 -0400

    api_service updates

--- services/api_service/app.py
+++ services/api_service/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask import request
 from flask import jsonify
+import json
+import os
 
 #global variables oxygen level
 oxygen_level = 19.5
@@ -10,6 +12,30 @@ app = Flask(__name__)
 @app.route('/api/v1/oxygen', methods=['GET'])
 def get_oxygen():
 	return jsonify({"oxygen_level": oxygen_level})
+
+@app.route('/api/v1/temperature', methods=['GET'])
+def get_temperature():
+	# load temperatures from json file (temperatures.json) and return json response
+
+	# Define the absolute path to the file
+	file_path = os.path.join(os.path.dirname(__file__), 'temperatures.json')
+
+	# read file
+	with open(file_path, 'r') as file:
+		data = file.read()
+	
+	# parse file
+	temperatures = json.loads(data)
+	return jsonify(temperatures)
+
+@app.route('/api/v1/camera1', methods=['GET'])
+def get_camera1():
+    # Define the absolute path to the file
+    file_path = os.path.join(os.path.dirname(__file__), 'cam1.png')
+
+    # Return the image file
+    return send_file(file_path, mimetype='image/png')
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
